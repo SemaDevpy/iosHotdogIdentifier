4e3d143dcd9e07f730db8e7a78c31570dc866283
SemaDevpy <syimykdev@gmail.com>
2020-06-15 16:22:17 +0600

    Initial Commit

---
+++ SeaFood/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SeaFood
+//
+//  Created by Syimyk on 6/15/20.
+//  Copyright © 2020 Syimyk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
